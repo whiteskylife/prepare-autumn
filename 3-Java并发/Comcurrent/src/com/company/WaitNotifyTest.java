0431b8b8c61f21ce6be4e4e84d19f4a95ca95fce
zjxu97 <zjxu97@126.com>
2019-08-12 00:45:28 +0800

    0812 Object的线程相关的方法

--- 3-Java并发/Comcurrent/src/com/company/WaitNotifyTest.java
+++ 3-Java并发/Comcurrent/src/com/company/WaitNotifyTest.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,28 +15,33 @@ import java.util.concurrent.TimeUnit;
 public class WaitNotifyTest {
     public static void main(String[] args) {
         final Object lock = new Object();
-
-        new Thread(() -> {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //等待中的线程 - A
+        executorService.execute(() -> {
             synchronized (lock) {
                 try {
                     TimeUnit.SECONDS.sleep(1);
+                    System.out.println("A进入等待");
                     lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println("A结束等待");
             }
-        }).start();
-
-        new Thread(() -> {
+        });
+        //唤醒 - 等待中的线程 - B
+        executorService.execute(() -> {
             synchronized (lock) {
                 try {
-                    TimeUnit.SECONDS.sleep(5);
+                    TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println("B唤醒 - A的等待");
                 lock.notify();
             }
-        }).start();
+        });
+        executorService.shutdown();
     }
 }
 
