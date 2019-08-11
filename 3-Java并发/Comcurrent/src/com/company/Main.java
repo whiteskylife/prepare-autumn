b3686fb511e91e74024039cb65d96f46143fadd2
zjxu97 <zjxu97@126.com>
2019-08-11 15:28:53 +0800

    0811 synchronized复习

---
+++ 3-Java并发/Comcurrent/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * @author thisxzj
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ 3-Java并发/Comcurrent/src/com/company/SynchronizedTest.java
@@ -0,0 +1,65 @@
+
+package com.company;
+
+import java.util.concurrent.*;
+
+/**
+ * 中建
+ *
+ * @author thisxzj
+ * @date 2019 2019-08-11 14:27
+ */
+
+
+public class SynchronizedTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 3; i++) {
+            executorService.execute(new MyRunnable());
+        }
+        executorService.shutdown();
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        SynchronizedExample s = new SynchronizedExample();
+        try {
+            s.func();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SynchronizedExample {
+    public void func() throws InterruptedException {
+        synchronized (SynchronizedExample.class) {
+            for (int i = 0; i < 5; i++) {
+                long threadId = Thread.currentThread().getId();
+                System.out.println(threadId + " " + i + " ");
+                if (threadId % 2 == 0) {
+                    Thread.sleep(2);
+                }
+            }
+        }
+    }
+
+    static synchronized void func() throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            long threadId = Thread.currentThread().getId();
+            System.out.println(threadId + " " + i + " ");
+            if (threadId % 2 == 0) {
+                Thread.sleep(2);
+            }
+        }
+    }
+}
+
+
+
+
+/*
+
+ */
